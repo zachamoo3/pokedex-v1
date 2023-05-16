038c30c21e396b562cf87ce43b2d168b6a0f057b
Zachary Moore <zachary.d.moore184@gmail.com>
2023-05-16 17:30:36 -0500

    adding beginning entries to pokemon list

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+pokemonList = [
+    {
+        name: bulbasaur,
+        pokemonNumber: 1,
+        type: ['grass', 'poison']
+    },
+    {
+        name: charmander,
+        pokemonNumber: 4,
+        type: 'fire'
+    },
+    {
+        name: squirtle,
+        pokemonNumber: 7,
+        type: 'water'
+    },
+    {
+        name: pikachu,
+        pokemonNumber: 25,
+        type: 'electric'
+    },
+    {
+        name: eevee,
+        pokemonNumber: 133,
+        type: 'normal'
+    },
+];
