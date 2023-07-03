15de21ed086b1e68f0509119484f90636078dc9f
Zachary Moore <zachary.d.moore184@gmail.com>
2023-07-02 20:00:42 -0500

    adding accessibility to modal and navbar

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 let pokemonRepository = (function () { //wrapping the pokemonList inside of an IIFE (Immediately Invoked Function Expression)
     let pokemonList = [];
     let api = [
-        {url: 'https://pokeapi.co/api/v2/pokemon/?limit=151', gen: 'I', offset: 0},
-        {url: 'https://pokeapi.co/api/v2/pokemon/?offset=151&limit=100', gen: 'II', offset: 151},
-        {url: 'https://pokeapi.co/api/v2/pokemon/?offset=251&limit=135', gen: 'III', offset: 251},
-        {url: 'https://pokeapi.co/api/v2/pokemon/?offset=386&limit=107', gen: 'IV', offset: 386},
-        {url: 'https://pokeapi.co/api/v2/pokemon/?offset=493&limit=156', gen: 'V', offset: 493},
-        {url: 'https://pokeapi.co/api/v2/pokemon/?offset=649&limit=72', gen: 'VI', offset: 649},
-        {url: 'https://pokeapi.co/api/v2/pokemon/?offset=721&limit=88', gen: 'VII', offset: 721},
-        {url: 'https://pokeapi.co/api/v2/pokemon/?offset=809&limit=96', gen: 'VIII', offset: 809},
-        {url: 'https://pokeapi.co/api/v2/pokemon/?offset=905&limit=105', gen: 'IX', offset: 905}
+        { url: 'https://pokeapi.co/api/v2/pokemon/?limit=151', gen: 'I', offset: 0 },
+        { url: 'https://pokeapi.co/api/v2/pokemon/?offset=151&limit=100', gen: 'II', offset: 151 },
+        { url: 'https://pokeapi.co/api/v2/pokemon/?offset=251&limit=135', gen: 'III', offset: 251 },
+        { url: 'https://pokeapi.co/api/v2/pokemon/?offset=386&limit=107', gen: 'IV', offset: 386 },
+        { url: 'https://pokeapi.co/api/v2/pokemon/?offset=493&limit=156', gen: 'V', offset: 493 },
+        { url: 'https://pokeapi.co/api/v2/pokemon/?offset=649&limit=72', gen: 'VI', offset: 649 },
+        { url: 'https://pokeapi.co/api/v2/pokemon/?offset=721&limit=88', gen: 'VII', offset: 721 },
+        { url: 'https://pokeapi.co/api/v2/pokemon/?offset=809&limit=96', gen: 'VIII', offset: 809 },
+        { url: 'https://pokeapi.co/api/v2/pokemon/?offset=905&limit=105', gen: 'IX', offset: 905 }
     ];
     let currentApi = api[0];
 
@@ -22,16 +22,29 @@ let pokemonRepository = (function () { //wrapping the pokemonList inside of an I
     function addNavItem(item) { //used to add links and options to the navbar
         let navbarNav = document.querySelector('.navbar-nav');
         let navItem = document.createElement('li');
-        let link = document.createElement('button');
+        let button = document.createElement('button');
 
-        $(link).addClass('nav-link btn btn-link').text(`Gen. ${item.gen} Pok${'\xE9'}mon`)
-        .on('click', function() {
-            changeApi(item);
-        });
+        $(button).addClass('nav-link btn btn-link').text(`Gen. ${item.gen} Pok${'\xE9'}mon`)
+            .on('click', function () {
+                let current = document.querySelector('span.sr-only');
+                $(current).remove();
+
+                changeApi(item);
+
+                let span = document.createElement('span');
+                $(span).addClass('sr-only').text('(current)');
+                button.appendChild(span);
+            });
+
+        if (currentApi.offset === item.offset) {
+            let span = document.createElement('span');
+            $(span).addClass('sr-only').text('(current)');
+            button.appendChild(span);
+        }
 
         $(navItem).addClass('nav-item');
 
-        navItem.appendChild(link);
+        navItem.appendChild(button);
         navbarNav.appendChild(navItem);
     }
 
@@ -171,17 +184,21 @@ let pokemonRepository = (function () { //wrapping the pokemonList inside of an I
             console.log(pokemon);
 
             let modalContainer = document.querySelector('.modal-container');
-            $(modalContainer).addClass('modal').html('');
+            $(modalContainer).addClass('modal').attr('aria-labelledby', 'Pokemon details').attr('aria-hidden', 'true')
+            .attr('role', 'dialog').html('');
 
             let modalDialog = document.createElement('div');
-            $(modalDialog).addClass('modal-dialog');
+            $(modalDialog).attr('role', 'document').addClass('modal-dialog');
 
             let modalContent = document.createElement('div');
             $(modalContent).addClass('modal-content');
 
             let closeButtonElement = document.createElement('button');
-            $(closeButtonElement).addClass('modal-close close').attr('data-dismiss','modal').text('Close');
+            $(closeButtonElement).addClass('modal-close close').attr('data-dismiss', 'modal').attr('aria-label', 'close');
             closeButtonElement.addEventListener('click', hideDetails());
+
+            let ariaSpan = document.createElement('span');
+            $(ariaSpan).attr('aria-hidden', 'true').text('Close');
 
             let titleElement = document.createElement('h1');
             titleElement.innerText = `${pokemon.name.charAt(0).toUpperCase()}${pokemon.name.slice(1)} #${pokemon.id}`;
@@ -212,6 +229,7 @@ let pokemonRepository = (function () { //wrapping the pokemonList inside of an I
                 `;
             }
 
+            closeButtonElement.appendChild(ariaSpan);
             modalContent.appendChild(closeButtonElement);
             modalContent.appendChild(titleElement);
             modalContent.appendChild(spriteElement);
@@ -243,7 +261,7 @@ let pokemonRepository = (function () { //wrapping the pokemonList inside of an I
         let button = document.createElement('button');
 
         button.innerText = `#${pokemon.id} ${pokemon.name.charAt(0).toUpperCase()}${pokemon.name.slice(1)}`;
-        $(button).addClass('list-button btn btn-primary').attr('data-toggle', 'modal').attr('data-target','.modal-container');
+        $(button).addClass('list-button btn btn-primary').attr('data-toggle', 'modal').attr('data-target', '.modal-container');
         button.addEventListener('click', function () {
             showDetails(pokemon);
         })
